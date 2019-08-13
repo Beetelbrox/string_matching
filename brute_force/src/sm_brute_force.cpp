561757ba0c147a4b124d618bce3f4a5d634014bd
Beetelbrox <beetelbrox@gmail.com>
2019-08-13 10:11:23 +0200

    Fix some formatting

--- brute_force/src/sm_brute_force.cpp
+++ brute_force/src/sm_brute_force.cpp
@@ -4,12 +4,12 @@
 int parse_input (int argc, char* argv[], std::string &pattern, std::string &sequence);
 bool check_match (int i, const std::string &pattern, const std::string &sequence);
 
-int main (int argc, char** argv) {
+int main ( int argc, char** argv ) {
   std::string pattern, sequence;
   if ( parse_input(argc, argv, pattern, sequence ) < 0 ) return -1;
 
-  for (size_t i = 0; i <= sequence.length()-pattern.length(); ++i) {
-    if ( check_match(i, pattern, sequence) ) {
+  for ( size_t i = 0; i <= sequence.length()-pattern.length(); ++i ) {
+    if ( check_match( i, pattern, sequence ) ) {
       std::cout << i << std::endl;
       return 0;
     }
@@ -18,20 +18,20 @@ int main (int argc, char** argv) {
   return 0;
 }
 
-int parse_input(int argc, char* argv[], std::string &pattern, std::string &sequence) {
+int parse_input( int argc, char* argv[], std::string &pattern, std::string &sequence ) {
   if ( argc == 1 ) {
-    if (!std::getline(std::cin, pattern)) {
+    if ( !std::getline( std::cin, pattern ) ) {
       std::cerr << "Error: Missing pattern input." << std::endl;
       return -1;
     }
-    if (!std::getline(std::cin, sequence)) {
+    if ( !std::getline(std::cin, sequence ) ) {
       std::cerr << "Error: Missing sequnce input." << std::endl;
       return -1;
     }
-  }else if (argc == 3 ){
+  } else if ( argc == 3 ){
     pattern = argv[1];
     sequence = argv[2];
-  }else {
+  } else {
     std::cerr << "Error: Wrong number of parameters" << std::endl;
     return -1;
   }
@@ -39,8 +39,8 @@ int parse_input(int argc, char* argv[], std::string &pattern, std::string &seque
 }
 
 bool check_match (int i, const std::string &pattern, const std::string &sequence){
-  for(size_t ii=0; ii < pattern.length(); ++ii ) {
-    if (sequence[i+ii] != pattern[ii]) return false;
+  for ( size_t ii=0; ii < pattern.length(); ++ii ) {
+    if ( sequence[i+ii] != pattern[ii] ) return false;
   }
   return true;
 }
